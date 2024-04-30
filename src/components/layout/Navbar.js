f05b433e06b24899489d4e1555a196c6ae448447
Object-ions <moshikoatia@gmail.com>
2024-04-30 15:55:30 -0700

    Successfully style Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="navbar" style={{ height: '100%' }}>
-      Navbar
-    </div>
+    <ul className="navbar">
+      <li>
+        <Link to={'/about'}>
+          <small>{'>'} </small>
+          About
+        </Link>
+      </li>
+      <li>
+        <Link to={'/'}>
+          <small>{'>'} </small>
+          Programming
+        </Link>
+      </li>
+      <li>
+        <Link to={'/design'}>
+          <small>{'>'} </small>
+          Design
+        </Link>
+      </li>
+    </ul>
   );
 };
 
