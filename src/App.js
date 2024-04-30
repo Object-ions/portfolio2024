50ef441d61cd7f729930a567f9dde000d8fc2a70
Object-ions <moshikoatia@gmail.com>
2024-04-30 14:07:35 -0700

    Place very basic layout of the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Header from './components/layout/Header';
+import Programming from './pages/Programming';
+import About from './pages/About';
+import Design from './pages/Design';
+import Footer from './components/layout/Footer';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <>
+      <Router>
+        <div className="page">
+          <Header />
+
+          <main className="main">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Programming />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/design" element={<Design />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navbar" style={{ height: '100%' }}>
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
