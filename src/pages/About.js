2d57a7728e589f8ee424eba20c7a2f3900d4f645
Object-ions <moshikoatia@gmail.com>
2024-05-01 18:53:21 -0700

    Add About and its presentation, update fonts across the app

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h2>
+        Creativity is my secret weapon and I’m always eager to collaborate.
+      </h2>
+      <div className="about-text">
+        <p>
+          My journey began with a deep appreciation for aesthetics, leading me
+          to study Product Design at 'Shenkar College of Engineering and
+          Design'. This background has given me a keen eye for detail and a
+          solid foundation in creating user-friendly designs.
+        </p>
+
+        <p>
+          As my fascination with user experience grew through my transition from
+          physical to digital products, it naturally led me to pursue a career
+          in full-stack development. In 2024, I graduated from Epicodus,
+          specializing in React and C#. My technical arsenal includes HTML, CSS,
+          JavaScript, Node.js, ASP.NET, MySQL, Firebase, and more.
+        </p>
+        <p>
+          I thrive on challenges and am always looking for new opportunities to
+          learning.
+        </p>
+      </div>
+      <div className="about-contact">
+        <h3>Let’s create something together</h3>
+        <p>@ : moshikatia@gmail.com</p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
