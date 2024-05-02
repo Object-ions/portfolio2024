2d8c846b78fbc13c043b781bbf3114c89f43ab54
Object-ions <moshikoatia@gmail.com>
2024-05-02 14:49:55 -0700

    Update links for design portfolio

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Design = () => {
-  return <div>Design</div>;
+  return (
+    <div>
+      <h3 style={{ lineHeight: '2' }}>
+        This is still a work in progress, but you can check out my previous
+        design portfolio if you{' '}
+        <a
+          href="https://object-ions.wixsite.com/portfolio/new-home"
+          style={{ background: '#41e0d0', color: '#000', padding: '5px' }}
+          alt="design portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Here
+        </a>
+      </h3>
+    </div>
+  );
 };
 
 export default Design;
