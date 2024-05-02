7c5c89ad7a12312f308e20c6824185a8e0a2f792
Object-ions <moshikoatia@gmail.com>
2024-05-02 01:10:51 -0700

    Add interval to change the name

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,6 +4,7 @@ import NavbarLinks from './NavbarLinks';
 
 const Header = () => {
   const [windowLength, setWindowLength] = useState(window.innerWidth);
+  const [displayName, setDisplayName] = useState('Moses Poston');
 
   useEffect(() => {
     const handleResize = () => {
@@ -17,11 +18,51 @@ const Header = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const replacements = {
+      M: 'ẅ',
+      ẅ: 'M',
+      o: '•',
+      '•': 'o',
+      s: 'З',
+      З: 's',
+      e: '&',
+      '&': 'e',
+      P: 'þ',
+      þ: 'P',
+      n: 'ň',
+      ň: 'n',
+      t: 'Ł',
+      Ł: 't',
+    };
+
+    const timer = setInterval(() => {
+      const characters = Array.from(displayName);
+      const targets = Object.keys(replacements);
+
+      // Select a random target character from the list that exists in the current displayName
+      const validTargets = targets.filter((char) => characters.includes(char));
+      if (validTargets.length > 0) {
+        const randomChar =
+          validTargets[Math.floor(Math.random() * validTargets.length)];
+        const indexToReplace = characters.findIndex(
+          (char) => char === randomChar
+        );
+
+        // Replace the character with its corresponding replacement
+        characters[indexToReplace] = replacements[randomChar];
+        setDisplayName(characters.join(''));
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [displayName]);
+
   return (
     <div className="header">
       <Link to="/">
         <h1>
-          <small>$</small> Moses Poston
+          <small>$</small> {displayName}
         </h1>
       </Link>
       <div className="header-links">
