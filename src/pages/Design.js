cb9f12f11b47aecfe5f38afdac38935f58a6dfdb
Object-ions <moshikoatia@gmail.com>
2024-05-17 09:55:53 -0700

    Update 'Design disclaimer and responsive bug on 'about'

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 const Design = () => {
   return (
-    <div>
-      <h3 style={{ lineHeight: '2' }}>
-        This is still a work in progress, but you can check out my previous
-        design portfolio if you{' '}
+    <div className="design">
+      <h3 style={{ lineHeight: '2' }}>This is a work in progress.</h3>
+      <p>
+        If you'd like to view my design portfolio, please{' '}
         <a
           href="https://object-ions.wixsite.com/portfolio/new-home"
-          style={{ background: '#41e0d0', color: '#000', padding: '5px' }}
+          style={{ background: '#41e0d0', color: '#000', padding: '2px' }}
           alt="design portfolio"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Click Here
+          click here.
         </a>
-      </h3>
+        <br />
+        Note: This website is best viewed on a desktop and may not be fully
+        supported on mobile devices.
+      </p>
     </div>
   );
 };
