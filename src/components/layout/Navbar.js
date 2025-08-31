e4727b73224a2adeca65bd745592499fbfca324c
Object-ions <moshikoatia@gmail.com>
2025-08-30 22:17:02 -0400

    fix: remove unused Link import in Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,8 @@
-import { Link } from 'react-router-dom';
 import NavbarLinks from './NavbarLinks';
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* <ul className="navbar-selection">
-        <li>
-          <Link to={'/'}>
-            <small>{'>'} </small>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to={'/programming'}>
-            <small>{'>'} </small>
-            Programming
-          </Link>
-        </li>
-        <li className="space"> </li>
-      </ul> */}
       <NavbarLinks />
     </div>
   );
